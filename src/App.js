889d2ccc43b92136adf462729956d8237210c456
Szymon Wojnar <wojnar48@gmail.com>
2019-11-13 21:59:09 -0500

    Refactor

--- src/App.js
+++ src/App.js
@@ -2,94 +2,99 @@ import React from 'react';
 import axios from 'axios';
 import { Route, Link } from 'react-router-dom';
 
-import Search from './components/Search'
-import config from './components/config'
-import GifList from './components/GifList'
-import Favorites from './components/Favorites'
+import Search from './components/Search';
+import config from './components/config';
+import GifList from './components/GifList';
+import Favorites from './components/Favorites';
 import './App.css';
-
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      gifs: [],
       searchValue: '',
-      results: [],
       isLoaded: false,
-      favorites: []
+      results: [],
+      favorites: [],
     };
-  this.getGifData = this.getGifData.bind(this);
-  this.handleSearchResultClick = this.handleSearchResultClick.bind(this)
-  this.handleChange = this.handleChange.bind(this);
-  this.handleFavorite = this.handleFavorite.bind(this);
-}
 
-async getGifData(event) {
-  event.preventDefault()
-  try {
-    const response = await axios.get(`${config.API_URL}?api_key=${config.API_TOKEN}&q=${this.state.searchValue}`)
-        this.setState({
-            results: response.data.data
-        })
-      }
-  catch (error) {
-    console.error(error);
+    this.getGifData = this.getGifData.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleFavorite = this.handleFavorite.bind(this);
   }
-}
 
-async handleSearchResultClick(gifs) {
-  await this.setState(state => ({
-    searchValue: [...state.searchValue, gifs]
-  }));
-  await this.getGifData(this.state.searchValue);
-}
+  async getGifData(event) {
+    event.preventDefault();
 
-handleChange(event) {
-  this.setState({
-    searchValue: event.target.value
-  });
-}
+    const { searchValue } = this.state;
 
-async handleFavorite(id) {
-  this.setState(state => ({
-    favorites: [...state.favorites, id]
-  }))
-}
+    try {
+      const response = await axios.get(
+        `${config.API_URL}?api_key=${config.API_TOKEN}&q=${searchValue}`,
+      );
 
-async componentDidMount() {
-  await this.setState(state => ({
-    isLoaded: !state.isLoaded
-  }));
-}
+      this.setState({
+        results: response.data.data,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-render() {
-  return (
-    <div className="App">
-      <div className="header">
-        <h1>Go Go Giphy</h1>
-      </div>
-      <div className="nav-bar">
-        <div className="home-link">
-          <Link to="/">Home</Link>
+  handleChange(event) {
+    this.setState({
+      searchValue: event.target.value,
+    });
+  }
+
+  async handleFavorite(id) {
+    this.setState((state) => ({
+      favorites: [...state.favorites, id],
+    }));
+  }
+
+  render() {
+    const { favorites, results } = this.state;
+
+    return (
+      <div className="App">
+        <div className="header">
+          <h1>Go Go Giphy</h1>
         </div>
+        <div className="nav-bar">
+          <div className="home-link">
+            <Link to="/">Home</Link>
+          </div>
           <div className="favorites-link">
             <Link to="/Favorites">Favorites</Link>
           </div>
+        </div>
+        <div className="search-container">
+          <Search
+            getGifData={this.getGifData}
+            handleChange={this.handleChange}
+          />
+        </div>
+        <div className="gif-list">
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <GifList
+                handleFavorite={this.handleFavorite}
+                gifs={results}
+                favorites={favorites}
+              />
+            )}
+          />
+          <Route
+            path="/Favorites"
+            render={() => <Favorites favorites={favorites} />}
+          />
+        </div>
       </div>
-      <div className="search-container">
-        <Search getGifData={this.getGifData} handleChange={this.handleChange}/>
-      </div>
-      <div className="gif-list">
-      { this.state.isLoaded
-          && <Route exact path="/" render={() =>
-            (<GifList handleFavorite={this.handleFavorite} gifs={this.state.results} favorites={this.state.favorites} />)} />
-      }
-        <Route path="/Favorites" render={() => (<Favorites favorites={this.state.favorites}/>)} />
-      </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default App;
--- src/components/GifList.jsx
+++ src/components/GifList.jsx
@@ -2,23 +2,18 @@ import React from 'react';
 import './GifList.css';
 
 const GifList = (props) => {
-  if (!props.gifs)
-  return
-  const gifs = props.gifs.map((gif,i) => {
-    const isFavorite = props.favorites.filter(FavoriteGif => FavoriteGif.id === gif.id).length > 0 
-    return(
+  if (!props.gifs) return;
+  const gifs = props.gifs.map((gif, i) => {
+    const isFavorite = props.favorites.filter((favGif) => favGif.id === gif.id).length > 0;
+    return (
       <div onClick={() => props.handleFavorite(gif)} key={i} className="image">
         <img id={gif.id} src={gif.images.fixed_width.url} className="image" alt={gif.images.slug} />
-        {isFavorite ? <span> &hearts; </span> : ""} 
+        {isFavorite ? <span> &hearts; </span> : ''}
       </div>
-    )
+    );
   });
-  return (
-    <div className="gif-list">
-      {gifs}
-    </div>
-  );
-};
 
+  return <div className="gif-list">{gifs}</div>;
+};
 
 export default GifList;
